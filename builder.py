70a57d43c5181940020869f317ed03aab7b333df
twchen88 <chloetwchen@gmail.com>
2024-05-16 18:33:18 -0500

    working masking

--- builder.py
+++
@@ -1,7 +0,0 @@
-<html>
-<head><title>302 Moved Temporarily</title></head>
-<body>
-<center><h1>302 Moved Temporarily</h1></center>
-<hr><center>Umbrella Cloud Security Gateway</center>
-</body>
-</html>
