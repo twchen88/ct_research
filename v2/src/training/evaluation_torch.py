f8b6585813d40addf95f33e64735bd829aab44f4
Chloe Chen <Chloe.Chen@cognizant.com>
2025-09-08 15:38:48 -0700

    Add function descriptions

--- v2/src/training/evaluation_torch.py
+++ v2/src/training/evaluation_torch.py
@@ -1,8 +1,20 @@
 import torch
 
-from src.training.training_torch import MAE, MSE
+from src.training.model_torch import Predictor
+from torch.utils.data import DataLoader
+from src.training.training_torch import MAE
 
-def predict(model, data_loader):
+def predict(model: Predictor, data_loader: DataLoader) -> torch.Tensor:
+    """
+    Generate predictions using the provided model and data loader.
+    
+    Parameters:
+        model: The trained PyTorch model.
+        data_loader: DataLoader providing the input data, usually a test dataset loader.
+
+    Returns:
+        torch.Tensor: The model predictions.
+    """
     model.eval()
 
     outputs = []
@@ -17,6 +29,7 @@ def predict(model, data_loader):
 def evaluate_error(predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     """
     Custom evaluation function that computes the mean absolute error between predictions and targets.
+    This function uses the MAE function defined in training_torch.py.
     
     Parameters:
         predictions (torch.Tensor): The model predictions.
