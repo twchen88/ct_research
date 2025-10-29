db403cc7477842a9662df329ebd8444785e49d68
Chloe Chen <Chloe.Chen@cognizant.com>
2025-10-29 09:51:44 -0700

    Add single-outcome predictor

--- src/ct/training/model_torch.py
+++ src/ct/training/model_torch.py
@@ -27,3 +27,23 @@ class Predictor(torch.nn.Module):
 
     def forward(self, x):
         return self.model(x)
+    
+
+class PredictorSingleOutcome(torch.nn.Module):
+    """
+    A simple neural network model for predicting a single target domain based on current scores and target domain encoding.
+    The model consists of two linear layers with a sigmoid activation function in between.
+    """
+    def __init__(self):
+        super().__init__()
+        n_domains = 14
+        
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(n_domains * 3, 100),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(100, 1),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
