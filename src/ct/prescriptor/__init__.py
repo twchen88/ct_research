82f4808e066092871f86d0c88a02abaa3a7a58dd
Chloe Chen <Chloe.Chen@cognizant.com>
2025-11-19 13:00:31 -0800

    Create __init__.py

---
+++ src/ct/prescriptor/__init__.py
@@ -0,0 +1,7 @@
+"""
+Prescriptor training and evaluation utilities.
+"""
+
+# from . import config_loading, evaluation_torch, file_io, model_torch, training_torch (placeholder, to be implemented)
+
+__all__ = []
